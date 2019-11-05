f454b9089e50dd3b21485b9aa40c6d10e625d704
SupportComeos <support@comeos.fr>
2019-11-06 00:33:23 +0100

    GET Enterprise avec filtre sur plusieurs entreprises

--- src/main/java/com/hikaad/global/controller/EntreprisesController.java
+++ src/main/java/com/hikaad/global/controller/EntreprisesController.java
@@ -5,6 +5,7 @@ import com.hikaad.global.repository.EntreprisesRepository;
 import com.hikaad.utils.HikaadLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -21,9 +22,25 @@ public class EntreprisesController {
     }
 
     @GetMapping("/Entreprises")
-    List<Entreprises> getAll() {
+    public List<Entreprises> getAll() {
         try {
             return entreprisesRepository.findAll();
+        } catch (Exception e) {
+            HikaadLogger.error(e.toString());
+            return Collections.emptyList();
+        }
+    }
+
+    @GetMapping("/Entreprises/{search}")
+    public List<Entreprises> getByFilter(@PathVariable String search) {
+        try {
+            Long[]      filter = new Long[search.split(",").length];
+            String[]    result = search.split(",");
+
+            for (int i = 0; i < result.length; i++)
+                filter[i] = Long.parseLong(result[i]);
+
+            return entreprisesRepository.findByFilter(filter);
         } catch (Exception e) {
             HikaadLogger.error(e.toString());
             return Collections.emptyList();
